54dff267b53a21699f3f7a8bc76a6bb3131c6772
Ankit <ankitmaurya20223@gmail.com>
2024-12-26 17:30:08 +0530

    Sorting and 5 hours of project completed

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { sortTypes } from "@/constants";
+import { usePathname, useRouter } from "next/navigation";
 
 const Sort = () => {
-  return (
-    <div>Sort</div>
-  )
-}
+  const path = usePathname();
+  const router = useRouter();
 
-export default Sort
+  const handleSort = (value: string) => {
+    router.push(`${path}?sort=${value}`);
+  };
+
+  return (
+    <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+      <SelectTrigger className="sort-select">
+        <SelectValue placeholder={sortTypes[0].value} />
+      </SelectTrigger>
+      <SelectContent className="sort-select-content">
+        {sortTypes.map((sort) => (
+          <SelectItem
+            key={sort.label}
+            value={sort.value}
+            className="shad-select-item"
+          >
+            {sort.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default Sort;
