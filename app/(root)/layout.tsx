d0dc4ab6bba342cd7ea09962b94b6315cfa1a014
Ankit <ankitmaurya20223@gmail.com>
2024-12-26 17:47:26 +0530

    Project Completed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,6 +6,8 @@ import { redirect } from "next/navigation";
 import { Toaster } from "@/components/ui/toaster";
 import React from "react";
 
+export const dynamic = "force-dynamic";
+
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
 
@@ -16,7 +18,7 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
       <Sidebar {...currentUser} />
       <section className="flex h-full flex-1 flex-col">
         <MobileNavigation {...currentUser} />
-        <Header userId = {currentUser.$id} accountId = {currentUser.accountId}/>
+        <Header userId={currentUser.$id} accountId={currentUser.accountId} />
         <div className="main-content">{children}</div>
       </section>
 
